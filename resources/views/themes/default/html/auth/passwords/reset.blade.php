cba7247880af9a159d2a2dd5aa8a85c24da29c89
Artem Kolombet <kolombetam@gmail.com>
2017-06-21 21:28:41 +0300

    implemented password reset

--- resources/views/themes/default/html/auth/passwords/reset.blade.php
+++ resources/views/themes/default/html/auth/passwords/reset.blade.php
@@ -1,69 +1,63 @@
-@extends('layouts.app')
+@extends('themes.default.index')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+@section('template')
+    <div class="container">
+        <div class="row">
+            <div class="col m6 offset-m3">
+                <h4>Reset Password</h4>
 
-                <div class="panel-body">
+                <div class="row">
                     @if (session('status'))
-                        <div class="alert alert-success">
+                        <div>
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.request') }}">
+                    <form class="col s12" method="POST" action="{{ route('password.request') }}">
                         {{ csrf_field() }}
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
-
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="email" type="email" class="validate" name="email" value="{{ $email or old('email') }}" required autofocus>
+                                <label for="email" data-error="Incorrect email address">E-Mail Address</label>
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                    <span class="red-text">
+                                        {{ $errors->first('email') }}
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="password" type="password" class="validate" name="password" required>
+                                <label for="password">Password</label>
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
+                                    <span class="red-text">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
+                        <div class="rpw">
+                            <div class="input-field col-md-6">
+                                <input id="password-confirm" type="password" class="validate" name="password_confirmation" required>
+                                <label for="password-confirm">Confirm Password</label>
                                 @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
+                                    <span class="red-text">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Reset Password
+                        <div class="row">
+                            <div class="col s12">
+                                <button type="submit" class="btn">
+                                    Reset password
                                 </button>
                             </div>
                         </div>
@@ -72,5 +66,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
