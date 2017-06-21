8866634e6d7bc97b0704b32b92114779c6c3aca3
Artem Kolombet <kolombetam@gmail.com>
2017-06-21 11:51:39 +0300

    forgot password - send email

--- resources/views/themes/default/html/auth/passwords/email.blade.php
+++ resources/views/themes/default/html/auth/passwords/email.blade.php
@@ -1,46 +1,44 @@
-@extends('layouts.app')
+@extends('themes.default.index')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
+@section('template')
+    <div class="container">
+        <div class="row">
+            <div class="col m6 offset-m3">
+                <h4>Reset Password</h4>
+                <div class="row">
                     @if (session('status'))
-                        <div class="alert alert-success">
+                        <div>
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+                    <form class="col s12" method="POST" action="{{ route('password.email') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required>
+                                <label for="email" data-error="Incorrect email address">E-Mail Address</label>
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                    <span class="red-text">
+                                        {{ $errors->first('email') }}
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="row">
+                            <div class="col s12">
+                                <button type="submit" class="btn">
+                                    <i class="material-icons left">email</i>
                                     Send Password Reset Link
                                 </button>
                             </div>
                         </div>
                     </form>
                 </div>
+
             </div>
         </div>
     </div>
-</div>
 @endsection
