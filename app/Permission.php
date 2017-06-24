afe5506f506af0968f52c896191662a0792bae8e
Artem Kolombet <kolombetam@gmail.com>
2017-06-24 23:57:19 +0300

    CRUD for permission

--- app/Permission.php
+++ app/Permission.php
@@ -5,7 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Permission extends Model
-{
+class Permission extends Model{
     use SoftDeletes;
+
+    protected $fillable = ['name'];
 }
---
+++ resources/views/themes/admin/html/permission/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('themes.admin.index')
+
+@section('file_css')
+@stop
+
+@section('file_js')
+    <script>
+        jQuery(document).ready(function () {
+            jQuery('select').material_select();
+        });
+
+    </script>
+@stop
+
+@section('template')
+
+    <div class="row">
+        <div class="col s12 left-align">
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <h2>Edit permission</h2>
+
+            <form class="col s12" method="post" action="{{ route('permissions.update', $permission->id) }}">
+
+                {{csrf_field()}}
+                <input name="_method" type="hidden" value="PUT">
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input type="text" name="name" id="name" value="{{$permission->name}}">
+                        <label for="name">Name</label>
+                    </div>
+                </div>
+
+                
+                <div class="row">
+                    <br>
+                    <button class="btn waves-effect waves-light" type="submit" name="action">
+                        Save
+                        <i class="material-icons left">save</i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@stop
