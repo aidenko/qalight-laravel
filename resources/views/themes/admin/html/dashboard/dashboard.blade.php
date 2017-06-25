2c6d40d20570df17ff02361e16a80b064aaef82a
Artem Kolombet <kolombetam@gmail.com>
2017-06-25 12:38:36 +0300

    implemented admin access over middleware "admin";
    TODO: check if admin

---
+++ resources/views/themes/admin/html/dashboard/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('themes.admin.index')
+
+@section('file_css')
+@stop
+
+@section('file_js')
+@stop
+
+@section('template')
+
+    <div class="row">
+        <div class="col s12">
+            Hello!
+        </div>
+    </div>
+
+@stop
