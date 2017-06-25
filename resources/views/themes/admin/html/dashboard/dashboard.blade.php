d79e6a36fc07d4f468b80b6d270e45abba105484
Artem Kolombet <kolombetam@gmail.com>
2017-06-25 12:44:29 +0300

    logout function for admins

--- resources/views/themes/admin/html/dashboard/dashboard.blade.php
+++ resources/views/themes/admin/html/dashboard/dashboard.blade.php
@@ -13,5 +13,22 @@
             Hello!
         </div>
     </div>
+    <div class="row">
+        <div class="col s12">
+            <ul>
+                <li>
+                    <form method="post" action="{{route('admin.logout')}}">
+
+                        {{csrf_field()}}
+
+                        <button type="submit" class="btn">
+                            Logout
+                            <i class="material-icons left">pan_tool</i>
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
 
 @stop
